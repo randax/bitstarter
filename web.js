154b5d754508afcce0c00cc60c86fe3d0c55c0b4
Oyvind Randa <Oyvind.Randa@gmail.com>
2013-07-12 21:16:59 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    var fs = require('fs');
+    var output = '';
+    var buffer = Buffer(fs.readFileSync('index.html'));
+    
+    response.send(buffer.toString("utf-8"));
 });
 
+
+ 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+//  console.log("Listening on " + port);
 });
